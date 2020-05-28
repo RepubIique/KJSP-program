7513baaae241f31d03e0dd829c075bd303971967
Republique <kendrickbong1996@gmail.com>
2020-05-28 22:10:34 +0800

    Bargraph kinda wotking yay

--- frontend/src/view/home/HomeMixChartOne.js
+++ frontend/src/view/home/HomeMixChartOne.js
@@ -3,103 +3,22 @@ import { Bar } from 'react-chartjs-2';
 import { i18n } from 'i18n';
 import barChartData from './data/barChartdata.js';
 
-
 let result = [];
 
-
-
-
-
-
-
-
-
-const options = {
-  responsive: true,
-  tooltips: {
-    mode: 'label',
-  },
-  elements: {
-    line: {
-      fill: false,
-    },
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false,
-        gridLines: {
-          display: false,
-        },
-      },
-    ],
-    yAxes: [
-      {
-        type: 'linear',
-        display: false,
-        position: 'left',
-        id: 'y-axis-1',
-        gridLines: {
-          display: false,
-        },
-      },
-      {
-        type: 'linear',
-        display: false,
-        position: 'right',
-        id: 'y-axis-2',
-        gridLines: {
-          display: false,
-        },
-      },
-    ],
-  },
+let populateName = () => {
+  let nameArr = [];
+  for (let i = 0; i < result.length; i++) {
+    nameArr.push(result[i].workerName);
+  }
+  return nameArr;
 };
 
-
-let labelArray =[];
-
-for (let i = 0; i < result.length; i++) {
-  labelArray.push(result[i].workerName)
-  console.log(result[i].workerName)
-}
-
-
-
-
-
-const data1 = {
-  labels: [
-
-
-
-  ],
-  datasets: [
-    // {
-    //   label: i18n('home.charts.orange'),
-    //   type: 'line',
-    //   data: [51, 65, 40, 49, 60, 37, 40],
-    //   fill: false,
-    //   borderColor: '#EC932F',
-    //   backgroundColor: '#EC932F',
-    //   pointBorderColor: '#EC932F',
-    //   pointBackgroundColor: '#EC932F',
-    //   pointHoverBackgroundColor: '#EC932F',
-    //   pointHoverBorderColor: '#EC932F',
-    //   yAxisID: 'y-axis-2',
-    // },
-    {
-      type: 'bar',
-      label: i18n('home.charts.blue'),
-      data: [76, 43],
-      fill: false,
-      backgroundColor: '#36A2EB',
-      borderColor: '#36A2EB',
-      hoverBackgroundColor: '#36A2EB',
-      hoverBorderColor: '#36A2EB',
-      yAxisID: 'y-axis-1',
-    },
-  ],
+let populateQuan = () => {
+  let QuanArr = [];
+  for (let i = 0; i < result.length; i++) {
+    QuanArr.push(result[i].totalQuantity);
+  }
+  return QuanArr;
 };
 
 export default class HomeMixChartOne extends Component {
@@ -110,13 +29,85 @@ export default class HomeMixChartOne extends Component {
       result: [result],
     };
   }
-  
+
   async componentDidMount() {
     result = await barChartData.getData();
-    console.log(result);
+
     this.setState({ result: result });
   }
+
   render() {
+    const options = {
+      responsive: true,
+      tooltips: {
+        mode: 'label',
+      },
+      elements: {
+        line: {
+          fill: false,
+        },
+      },
+      scales: {
+        xAxes: [
+          {
+            display: false,
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            type: 'linear',
+            display: false,
+            position: 'left',
+            id: 'y-axis-1',
+            gridLines: {
+              display: false,
+            },
+          },
+          {
+            type: 'linear',
+            display: false,
+            position: 'right',
+            id: 'y-axis-2',
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+      },
+    };
+    const data1 = {
+      labels: populateName(),
+      datasets: [
+        {
+          label: i18n('home.charts.orange'),
+          type: 'line',
+          data: populateQuan(),
+          fill: false,
+          borderColor: '#EC932F',
+          backgroundColor: '#EC932F',
+          pointBorderColor: '#EC932F',
+          pointBackgroundColor: '#EC932F',
+          pointHoverBackgroundColor: '#EC932F',
+          pointHoverBorderColor: '#EC932F',
+          yAxisID: 'y-axis-2',
+        },
+        {
+          type: 'bar',
+          label: i18n('home.charts.blue'),
+          data: populateName(),
+          fill: false,
+          backgroundColor: '#36A2EB',
+          borderColor: '#36A2EB',
+          hoverBackgroundColor: '#36A2EB',
+          hoverBorderColor: '#36A2EB',
+          yAxisID: 'y-axis-1',
+        },
+      ],
+    };
+
     return <Bar data={data1} options={options} />;
   }
 }
