324048b225969ec9808f1cef647012158eb172a5
tosmo <timothy.osm@gmail.com>
2020-06-13 01:03:45 +0800

    polar chart added backend and bit of front end

--- frontend/src/view/home/HomePolarChart.js
+++ frontend/src/view/home/HomePolarChart.js
@@ -1,32 +1,66 @@
 import React, { Component } from 'react';
 import { Polar } from 'react-chartjs-2';
 import { i18n } from 'i18n';
-
-const data = {
-  datasets: [
-    {
-      data: [11, 16, 7, 3, 14],
-      backgroundColor: [
-        '#FF6384',
-        '#4BC0C0',
-        '#FFCE56',
-        '#E7E9ED',
-        '#36A2EB',
-      ],
-    },
-  ],
-  label: 'Ringgit Earned for the month',
-
-  labels: [
-    i18n('home.charts.red'),
-    i18n('home.charts.green'),
-    i18n('home.charts.yellow'),
-    i18n('home.charts.grey'),
-    i18n('home.charts.blue'),
-  ],
+import polarChartData from './data/polarChartdata.js';
+let result = [];
+let populateSub = () => {
+  let data = [];
+  for (let i = 0; i < result.length; i++) {
+    data.push(result[i].subdivision);
+  }
+  console.log(data);
+  return data;
 };
 
+let populateGross = () => {
+  let data = [];
+  for (let i = 0; i < result.length; i++) {
+    data.push(result[i].gross);
+  }
+  console.log(data);
+  return data;
+};
+
+
+let data = {
+
+};
 export default class HomePolarChart extends Component {
+  constructor(props) {
+    super(props); //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = {
+      //state is by default an object
+      result: [result],
+    };
+  }
+
+  async componentDidMount() {
+
+    result = await polarChartData.getData();
+    console.log(result);
+    this.setState({ result: result });
+    data = {
+      datasets: [
+        {
+          data: populateGross(),
+          backgroundColor: [
+            '#FF6384',
+            '#4BC0C0',
+            '#FFCE56',
+            '#E7E9ED',
+            '#36A2EB',
+          ],
+        },
+      ],
+      label: 'Ringgit Earned for the month',
+    
+      labels: [
+        populateSub()
+      ],
+    };
+  }
+
+  
   render() {
     return <Polar data={data} />;
   }
