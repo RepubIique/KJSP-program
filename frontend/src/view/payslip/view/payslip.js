49674d0b14b46f91ce14c57a50a7c2b43709f085
Republique <kendrickbong1996@gmail.com>
2020-06-16 19:39:26 +0800

    payslip corrected

--- frontend/src/view/payslip/view/payslip.js
+++ frontend/src/view/payslip/view/payslip.js
@@ -323,11 +323,11 @@ export default class payslip extends Component {
                           <br></br>
                           {$.totalOnLeave}
                           <br></br>
-                          {$.totalRestDay}
+                          {$.totalOvertimeHour}
                           <br></br>
                           {$.totalWorkingDay}
                           <br></br>
-                          {$.totalOvertimeHour}
+                          {$.totalWorkingHours}
                           <br></br>
                           {$.averageWorkingHour}
                         </Grid>
