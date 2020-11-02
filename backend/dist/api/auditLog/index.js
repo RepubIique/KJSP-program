6a1c8038cf77005401a71e9ba87ae64e5dcad19b
Kendrick Bong <54446138+RepubIique@users.noreply.github.com>
2020-11-02 12:48:37 +0800

    Revert "React revert"

--- backend/dist/api/auditLog/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (app) => {
-    app.get(`/tenant/:tenantId/audit-log`, require('./auditLogList').default);
-};
-//# sourceMappingURL=index.js.map
--- backend/dist/middlewares/languageMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.languageMiddleware = void 0;
-function languageMiddleware(req, res, next) {
-    req.language = req.headers['accept-language'] || 'en';
-    return next();
-}
-exports.languageMiddleware = languageMiddleware;
-//# sourceMappingURL=languageMiddleware.js.map
