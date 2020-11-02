65b42daa816c4f403c82b15a7a3024c10e0d3a65
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-11-02 13:06:28 +0800

    Revert "React revert"
    This reverts commit 65d9f9c10e7b5ed2ab52b5678adf182f42bbc6ff.

--- backend/dist/api/settings/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (app) => {
-    app.put(`/tenant/:tenantId/settings`, require('./settingsSave').default);
-    app.get(`/tenant/:tenantId/settings`, require('./settingsFind').default);
-};
-//# sourceMappingURL=index.js.map
